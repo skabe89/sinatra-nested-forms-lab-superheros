47a63015b92821963c6449d40477ccea9e6bf326
Sean <skabe89@gmail.com>
2020-12-25 00:27:48 -0500

    commit tree 4dafe8060a94dd908ebfa1382c45c23e9baf7a8c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,18 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/team' do
+        team_params = params[:team]
+        @team = Team.new(team_params)
+
+        heros_params = team_params[:heros]
+        @heros = heros_params.each{|hero| Hero.new(hero)}
+        
+        erb :team
+    end
+
 end
